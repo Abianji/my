865635aa5c9cebe3e46670ba67026ab766b4a388
DDXX <whynow@qq.com>
2020-07-09 21:32:17 +0800

    页面

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,43 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+  <v-app id="inspire" style="background: #f4f6f9">
+    <AppBar></AppBar>
+
+    <v-main>
+      <v-sheet dark>
+        <v-img
+          src="https://cdn.pixabay.com/photo/2020/04/17/14/16/mountains-5055387_1280.jpg"
+          height="100vh"
+          width="100vw"
+        >
+          <v-container class="fill-height"></v-container>
+        </v-img>
+      </v-sheet>
+
+      <v-container>
+        <router-view></router-view>
+      </v-container>
+    </v-main>
+
+    <Footer></Footer>
+  </v-app>
 </template>
 
+<script>
+import AppBar from "./components/Layout/AppBar";
+import Footer from "./components/Layout/Footer";
+
+export default {
+  name: "App",
+
+  data: () => ({
+    dialog: false
+  }),
+  components: {
+    AppBar,
+    Footer
+  }
+};
+</script>
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
 </style>
---
+++ src/components/Layout/AppBar.vue
@@ -0,0 +1,89 @@
+<template>
+  <div>
+    <v-navigation-drawer
+      v-if="!$vuetify.breakpoint.lgAndUp"
+      v-model="drawer"
+      :clipped="$vuetify.breakpoint.lgAndUp"
+      app
+      color="#f5f5f5"
+    >
+      <v-list nav color="#f5f5f5">
+        <v-list-item
+          v-for="(item, i) in barItems"
+          :key="i"
+          link
+          :to="item.to"
+          :href="item.href"
+          :target="item.target"
+        >
+          <v-list-item-content>
+            <v-list-item-title>{{ item.title }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-app-bar fixed app elevate-on-scroll :clipped-left="$vuetify.breakpoint.lgAndUp">
+      <v-container>
+        <v-row align="center" justify="space-between">
+          <v-col class="d-flex align-center">
+            <v-app-bar-nav-icon @click.stop="drawer = !drawer" v-if="!$vuetify.breakpoint.lgAndUp" />
+            <v-toolbar-title style="cursor: pointer" class="font-weight-bold headline">
+              <v-icon large>mdi-home-roof</v-icon>
+              <span>Maple</span>
+            </v-toolbar-title>
+          </v-col>
+          <v-col class="text-right" v-if="$vuetify.breakpoint.lgAndUp">
+            <v-btn
+              text
+              v-for="(item, i) in barItems"
+              :key="i"
+              link
+              :to="item.to"
+              :href="item.href"
+              :target="item.target"
+            >{{ item.title }}</v-btn>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-app-bar>
+  </div>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    dialog: false,
+    drawer: null,
+    barItems: [
+      {
+        title: "Home",
+        href: "http://localhost:8080",
+        target: "_black",
+        to: "/"
+      },
+      {
+        title: "About",
+        href: "http://localhost:8080",
+        target: "_black",
+        to: "/About"
+      },
+      {
+        title: "Detail",
+        href: "http://localhost:8080",
+        target: "_black",
+        to: "/detail"
+      },
+      {
+        title: "Authors",
+        href: "http://localhost:8080",
+        target: "_black",
+        to: "/authors"
+      }
+    ]
+  })
+};
+</script>
+
+<style>
+</style>
